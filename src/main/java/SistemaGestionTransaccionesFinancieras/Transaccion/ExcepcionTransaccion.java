f2c0829a55b1e68165e787a925f22fdeb51899a4
ManuelViquez07 <manuelfranciscovb@gmail.com>
2025-06-06 12:31:58 -0600

    Update ExcepcionTransaccion.java

--- src/main/java/SistemaGestionTransaccionesFinancieras/Transaccion/ExcepcionTransaccion.java
+++ src/main/java/SistemaGestionTransaccionesFinancieras/Transaccion/ExcepcionTransaccion.java
@@ -1,7 +1,30 @@
 package SistemaGestionTransaccionesFinancieras.Transaccion;
 
-public class ExcepcionTransaccion extends RuntimeException {
-    public ExcepcionTransaccion(String message) {
-        super(message);
+public class ExcepcionTransaccion {
+    private String mensaje;
+
+    public ExcepcionTransaccion() {
+        this.mensaje = "";
     }
+
+    public boolean validarIngreso(double monto) {
+        if (monto < 0) {
+            mensaje = "El monto no puede ser negativo para un ingreso.";
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validarEgreso(double monto, double capitalDisponible) {
+        if (monto > capitalDisponible) {
+            mensaje = "El egreso excede el capital disponible.";
+            return false;
+        }
+        return true;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
 }
