fcd45d250778c9f5465f0da073a85f34e3303393
ManuelViquez07 <manuelfranciscovb@gmail.com>
2025-06-06 11:51:27 -0600

    Update Transaccion.java

--- src/main/java/SistemaGestionTransaccionesFinancieras/Transaccion/Transaccion.java
+++ src/main/java/SistemaGestionTransaccionesFinancieras/Transaccion/Transaccion.java
@@ -1,4 +1,34 @@
 package SistemaGestionTransaccionesFinancieras.Transaccion;
 
-public class Transaccion {
+public abstract class Transaccion {
+    protected double monto;
+    protected String descripcion;
+    protected String fecha;
+
+    public Transaccion(double monto, String descripcion, String fecha) {
+        this.monto = monto;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public abstract double calcularImpuesto();
+
+    public abstract boolean procesarTransaccion(double capitalActual, ExcepcionTransaccion validador);
+
+    @Override
+    public String toString() {
+        return String.format("Fecha: %s | Monto: %.2f | Descripción: %s", fecha, monto, descripcion);
+    }
 }
