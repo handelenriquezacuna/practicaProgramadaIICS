a136333d750f10f5209bd3522cd1bfe0949de897
Jennifer <jennihernandez421@gmail.com>
2025-06-10 21:25:49 -0600

    Agregado menú interactivo para gestionar transacciones

--- src/main/java/SistemaGestionTransaccionesFinancieras/Menu/MenuInteractivo.java
+++ src/main/java/SistemaGestionTransaccionesFinancieras/Menu/MenuInteractivo.java
@@ -1,4 +1,36 @@
 package SistemaGestionTransaccionesFinancieras.Menu;
+import java.util.Scanner;
 
 public class MenuInteractivo {
+
+    private MenuInteractivo sistema;
+
+    public void Menu(MenuInteractivo sistema) {
+        this.sistema = sistema;
+    }
+
+    public  void mostrarMenu() {
+        boolean menu = true;
+        Scanner sc = new Scanner(System.in);
+
+        boolean Menu = true;
+        while (menu) {
+            System.out.println("\n=== MENÚ DE TRANSACCIONES ===");
+            System.out.println("1. Registrar ingreso");
+            System.out.println("2. Registrar egreso");
+            System.out.println("3. Eliminar transacción");
+            System.out.println("4. Mostrar todas las transacciones");
+            System.out.println("5. Salir");
+            System.out.println("Opción: ");
+
+            String opcion = sc.nextLine();
+
+            switch (opcion) {
+                case "1":
+
+            }
+        }
+    }
+
+
 }
